632dec113b04d5dfaa90e7519daa5652359a5634
ticommerce <betomcc0109@gmail.com>
2026-01-20 23:54:36 -0600

    Update server.js

--- server.js
+++ server.js
@@ -123,8 +123,8 @@ app.post('/send', async (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
   client.initialize();
 });
